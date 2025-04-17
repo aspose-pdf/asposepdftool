df61bf5f49dcd1c7119a7481cf1a41ec662bea1b
Oleksandr Andriienko <Oleksandr.Andriienko@aspose.com>
2025-04-17 20:19:18 +0200

    Updated to version 25.4.0 with new archives and formula

--- asposepdftool.rb
+++ asposepdftool.rb
@@ -1,7 +1,7 @@
 class Asposepdftool < Formula
   desc "Aspose.PDF CLI App"
   homepage "https://github.com/aspose-pdf/asposepdftool"
-  version "25.3.0"
+  version "25.4.0"
   license :cannot_represent # This tool is distributed under a custom license. See LICENSE.txt in the repository for details.
 
   livecheck do
@@ -13,10 +13,10 @@ class Asposepdftool < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/aspose-pdf/asposepdftool/raw/refs/tags/v#{version}/asposepdftool_macos_arm64.tar.bz2"
-      sha256 "5372f8a411fe869477a7627f99562889b3ed49beaf067dfc43e317f63809d3d5"
+      sha256 "cb9fd6eb857334351fc580366c8ef09122eccd3ab57d6d4889f7144b63810e52"
     else
       url "https://github.com/aspose-pdf/asposepdftool/raw/refs/tags/v#{version}/asposepdftool_macos_x86_64.tar.bz2"
-      sha256 "c58ef997fda044bf5a8c860f8deaef259bb1618c801ae5f66623cd5ee12e68ff"
+      sha256 "76ed3b6fe10d040a73d5351d03adf016d2bf611274418a60da4e68858b948aba"
     end
   end
 
